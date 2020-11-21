27182e3e5f487ce8b6ba1db83745c92808f6e0cf
Rajvi Rana <rajvi.rana@mail.utoronto.ca>
2020-11-21 12:21:14 -0500

    added the section header with the line border, and fixed padding

--- src/Pages/Team/Components/About.js
+++ src/Pages/Team/Components/About.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import {Image} from "react-bootstrap";
+import React from "react";
+import { Image } from "react-bootstrap";
 import { Container, Row, Col } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../Team.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../Team.css";
 
 const About = (props) => {
-    return (
-      <div class="AboutContainer">
-        <Image src={props.profile_img} roundedCircle fluid/>
-        <h5 className="ProfileName">{props.name}</h5>
-        <p className="role">{props.role}</p>
-      </div>
-    )
-}
+  return (
+    <div class="AboutContainer">
+      <Image src={props.profile_img} roundedCircle fluid />
+      <h5 className="ProfileName">{props.name}</h5>
+      <p className="Role">{props.role}</p>
+    </div>
+  );
+};
 
 export default About;

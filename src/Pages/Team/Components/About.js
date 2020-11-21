34eb3f8faa26f63b53edb8ea7290b83c5ba3df72
Rajvi Rana <rajvi.rana@mail.utoronto.ca>
2020-11-21 11:29:06 -0500

    added temp image, fixed oval issue so now images are circular, and changed class components to functional

--- src/Pages/Team/Components/About.js
+++ src/Pages/Team/Components/About.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {Image} from "react-bootstrap";
 import { Container, Row, Col } from "react-bootstrap";
-import photo from "https://via.placeholder.com/100";
+// import photo from "https://via.placeholder.com/100";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const About = () => {
+
+
+const About = (props) => {
     return (
       <div class="container">
-        <Image src={photo} roundedCircle fluid/>
+        <Image src={props.profile_img} roundedCircle fluid/>
         <h4>name</h4>
-        <h4>hello</h4>
+        <p>hello</p>
       </div>
     )
 }
--- src/Pages/Team/Team.js
+++ src/Pages/Team/Team.js
@@ -2,19 +2,21 @@ import React from 'react';
 import About from "./Components/About";
 import { Container, Row, Col } from "react-bootstrap";
 
+const IMG = "https://via.placeholder.com/100";
+
 const Team = () => {
     return (
       <div class="container">
        <h1 style={{ textAlign: 'center' }}>Meet Our Team</h1>
         <Row>
           <Col xs={12} md={3} lg={2}>
-            <About/>
+            <About profile_img={IMG}/>
           </Col>
           <Col xs={12} md={3} lg={2}>
-            <About/>
+          <About profile_img={IMG}/>
           </Col>
           <Col xs={12} md={3} lg={2}>
-            <About/>
+            <About profile_img={IMG}/>
           </Col>
         </Row>
       </div>

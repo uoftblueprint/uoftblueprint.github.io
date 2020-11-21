b4591e5a7440373b90cf3adc67672ed5d3bb9a77
Elena Wang <elenawzy@gmail.com>
2020-11-21 00:40:27 -0500

    added dummy about pictures

--- src/Pages/Team/Components/About.js
+++ src/Pages/Team/Components/About.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Image} from "react-bootstrap";
+import photo from "../Images/stock-photo.jpeg";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const About = () => {
     return (
       <div class="container">
-        <div class="row">
-          <h1 style={{ textAlign: 'center' }}>about</h1>
-        </div>
+        <Image src={photo} roundedCircle fluid/>
+        <h3>name</h3>
+        <h3>hello</h3>
       </div>
     )
 }

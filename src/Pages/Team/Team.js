73cc170962da2d20452b71ba0f4dbd8157598d62
Rajvi Rana <rajvi.rana@mail.utoronto.ca>
2020-11-21 12:21:14 -0500

    added the section header with the line border, and fixed padding

--- src/Pages/Team/Team.js
+++ src/Pages/Team/Team.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React from "react";
 import About from "./Components/About";
 import { Container, Row, Col } from "react-bootstrap";
 
 const IMG = "https://via.placeholder.com/100";
 
 const Team = () => {
-    return (
-      <div class="container">
-       <h3 className="TeamTitle">Meet Our Team</h3>
+  return (
+    <div class="container">
+      <h3 className="TeamTitle">Meet Our Team</h3>
+      <div className="TeamContainer">
+        <h5 className="HeaderStyle">
+          <span className="Header">EXECUTIVE TEAM</span>
+        </h5>
         <Row>
           <Col xs={12} md={3} lg={2}>
-            <About profile_img={IMG} name={"Elena"} role={"Best Human"}/>
+            <About profile_img={IMG} name={"Elena"} role={"Best Human"} />
           </Col>
           <Col xs={12} md={3} lg={2}>
-          <About profile_img={IMG} name={"Rajvi"} role={"Also Best Human"}/>
+            <About profile_img={IMG} name={"Rajvi"} role={"Also Best Human"} />
           </Col>
           <Col xs={12} md={3} lg={2}>
-            <About profile_img={IMG} name={"Varun"} role={"Best Boi"}/>
+            <About profile_img={IMG} name={"Varun"} role={"Best Boi"} />
           </Col>
         </Row>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Team;

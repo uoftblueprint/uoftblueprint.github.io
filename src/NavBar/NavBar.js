05f8c6a7dc6579411702d7875c75ad8496b7dfa9
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2020-12-20 16:49:53 -0800

    restyled navbard to support mobile views

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import { MenuItems } from "./MenuItems"
 import banner from '../assets/banner.png'
 import "./NavBar.css"
@@ -18,11 +18,27 @@ class NavigationBar extends React.Component {
                 </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end" style={{ paddingTop: '8px' }}>
-                    <Nav className="ml-auto">
-                        <Link className="navlink" to="/team">Team</Link>
-                        <Link className="navlink" to="/chapters">Chapters</Link>
-                        {/* <Link className="navlink" to="/projects">Projects</Link> */}
-                    </Nav>
+                    {window.innerWidth < 480 ? (
+                         <NavDropdown title="Pages" id="basic-nav-dropdown">
+                            <NavDropdown.Item>
+                                <Link to="/team">Team</Link>
+                            </NavDropdown.Item>
+                            <NavDropdown.Item>
+                                <Link to="/chapters">Chapters</Link>
+                            </NavDropdown.Item>
+                            <NavDropdown.Item>
+                                <Link to="/projects">Projects</Link>
+                            </NavDropdown.Item>
+                            {/* <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
+                        </NavDropdown>
+                    ) : (
+                        <Nav className="ml-auto">
+                            <Link className="navlink" to="/team">Team</Link>
+                            <Link className="navlink" to="/chapters">Chapters</Link>
+                            <Link className="navlink" to="/projects">Projects</Link>
+                        </Nav>
+                    )}
                 </Navbar.Collapse>
             </Navbar>
         )

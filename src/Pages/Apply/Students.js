6cea13bb62400e89868e20ddeb4d3b863d9776d1
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2021-01-06 12:41:19 -0500

    added what bp value icons

--- src/Pages/Apply/Students.js
+++ src/Pages/Apply/Students.js
@@ -1,8 +1,26 @@
 import React, { useEffect } from "react";
 import { Row, Col, Card, Jumbotron, Container } from 'react-bootstrap';
-import toronto from '../../assets/toronto.png';
-import pharmacy from '../../assets/pharmacy.png';
+import standing from '../../assets/standing 8.png';
+import { CriteriaList } from './applyData';
 import './Apply.css';
+
+const Criteria = ({ title, description, image }) => {
+  return (
+    <Row>
+      <Col md="auto">
+        <img src={image} style={{ paddingTop: '1em', width: '2.5em', height: 'auto' }} />
+      </Col>
+      <Col md="6" style={{ paddingTop: '1em' }}>
+        <h5 style={{ fontWeight: '600' }}>
+          {title}
+        </h5>
+        <p style={{ color: 'grey', fontWeight: '400'}}>
+          {description}
+        </p>
+      </Col>
+    </Row>
+  )
+}
 
 const Students = () => {
   return (
@@ -11,18 +29,47 @@ const Students = () => {
         <Container fluid="md" className={"justify-content-center"}>
             <div style={{ paddingLeft: '5%', paddingTop: '1.5em', paddingBottom: '2em' }}>
                 <h1 style={{"font-size": "36px", color: '#FFFFFF' }}>
-                    Current Projects
+                    For Students
                 </h1>
                 <div style={{ minWidth: '30vw', width: '35em', maxWidth: '75vw', paddingBottom: '1em' }}>
-                    <h4 style={{ padding: "25px 0 5px 0", "font-size": "1.25rem", color: '#FFFFFF' }}  className={"text-responsive"}>
-                        If you are interested in working with us, click <a style={{ color: 'white' }}href="mailto:contact@uoftblueprint.org">here</a> 
-                        {' '}to contact us with your project idea!
+                    <h4 style={{ padding: "25px 0 5px 0", "font-size": "2rem", color: '#FFFFFF', fontWeight: '200' }}  className={"text-responsive"}>
+                      Joining Blueprint is the perfect way to use your skills to give back to the community. We accept applications at the beginning of every semester.
                     </h4>
                 </div>
-                {/* <a href="mailto:contact@uoftblueprint.org"><button class="contact-button">Let's get in touch!</button></a> */}
+                <div style={{ minWidth: '30vw', width: '35em', maxWidth: '75vw', paddingBottom: '1em' }}>
+                    <h5 style={{ padding: "25px 0 5px 0", "font-size": "1.2rem", color: '#FFFFFF' }}  className={"text-responsive"}>
+                      Our application for developers and designers are now closed for the 2020-2021 academic year. We will be recruiting again in Fall 2021!
+                    </h5>
+                </div>
             </div>
         </Container>
       </Jumbotron>
+      <Container fluid="md" style={{ paddingTop: "4rem", paddingBottom: '8em' }}>
+          <Row>
+            { window.innerWidth < 480 ? null : (
+              <Col md="3">
+                <img src={standing} style={{ paddingTop: '4em', paddingBottom: '4em', width: '15em', height: 'auto' }} />
+              </Col>
+            )}
+            <Col md="8">
+                <h1> 
+                    What We Look For
+                </h1>
+                <p style={{ paddingTop: '2em', color: 'grey', fontSize: '1.4em', fontWeight: '400' }}>
+                 We would love to accept every applicant if we could, but unfortunately we cannot manage the volume of people and administrative overhead while still fulfilling our mission as a club. As such, we use the following criteria to find the best applicants for our club.
+                </p>
+                {CriteriaList.map((criteria) => {
+                  return (
+                    <Criteria 
+                      title={criteria.title}
+                      description={criteria.description}
+                      image={criteria.image}
+                      />
+                  )
+                })}
+            </Col>
+          </Row>
+      </Container>
     </>
     
   )
---
+++ src/Pages/Apply/applyData.js
@@ -0,0 +1,39 @@
+import passion from '../../assets/passion.png';
+import growth from '../../assets/growth.png';
+import resourceful from '../../assets/resourceful.png';
+import attitude from '../../assets/attitude.png';
+import teamwork from '../../assets/teamwork.png';
+import culture from '../../assets/culture.png';
+
+export const CriteriaList = [
+  {
+    title: 'Passion',
+    description: 'Do you demonstrate a genuine passion for social good?',
+    image: passion
+  },
+  {
+    title: 'Growth',
+    description: 'Do you push yourself to grow in any setting? Do you seek out new ways to grow?',
+    image: growth
+  },
+  {
+    title: 'Resourceful',
+    description: 'Do you adapt and explore in order to overcome difficulties?',
+    image: resourceful
+  },
+  {
+    title: 'Attitude',
+    description: 'Do you demonstrate positive, hard-working attitude?',
+    image: attitude
+  },
+  {
+    title: 'Teamwork',
+    description: 'Do you display the ability to work well in a team?',
+    image: teamwork
+  },
+  {
+    title: 'Culture',
+    description: 'Do you value empowering non-profits to achieve their missions through tech?',
+    image: culture
+  }
+]

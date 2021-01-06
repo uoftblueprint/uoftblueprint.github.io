d176790c71df475d327bf98cab588f9a768e8573
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2021-01-06 13:09:04 -0500

    added image for student apply jumbotron

--- src/Pages/Apply/Students.js
+++ src/Pages/Apply/Students.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { Row, Col, Card, Jumbotron, Container } from 'react-bootstrap';
 import standing from '../../assets/standing 8.png';
+import team from '../../assets/team-build2.png';
 import { CriteriaList } from './applyData';
 import './Apply.css';
 
@@ -27,21 +28,28 @@ const Students = () => {
     <>
       <Jumbotron fluid style={{ "background-color": "#3E9CF3"}}>
         <Container fluid="md" className={"justify-content-center"}>
-            <div style={{ paddingLeft: '5%', paddingTop: '1.5em', paddingBottom: '2em' }}>
-                <h1 style={{"font-size": "36px", color: '#FFFFFF' }}>
-                    For Students
-                </h1>
-                <div style={{ minWidth: '30vw', width: '35em', maxWidth: '75vw', paddingBottom: '1em' }}>
-                    <h4 style={{ padding: "25px 0 5px 0", "font-size": "2rem", color: '#FFFFFF', fontWeight: '200' }}  className={"text-responsive"}>
-                      Joining Blueprint is the perfect way to use your skills to give back to the community. We accept applications at the beginning of every semester.
-                    </h4>
-                </div>
-                <div style={{ minWidth: '30vw', width: '35em', maxWidth: '75vw', paddingBottom: '1em' }}>
-                    <h5 style={{ padding: "25px 0 5px 0", "font-size": "1.2rem", color: '#FFFFFF' }}  className={"text-responsive"}>
-                      Our application for developers and designers are now closed for the 2020-2021 academic year. We will be recruiting again in Fall 2021!
-                    </h5>
-                </div>
-            </div>
+          <Row>
+            <Col md="auto">
+              <div style={{ paddingLeft: '5%', paddingTop: '1.5em', paddingBottom: '2em' }}>
+                  <h1 style={{"font-size": "36px", color: '#FFFFFF' }}>
+                      For Students
+                  </h1>
+                  <div style={{ minWidth: '30vw', width: '35em', maxWidth: '75vw', paddingBottom: '1em' }}>
+                      <h4 style={{ padding: "25px 0 5px 0", "font-size": "2rem", color: '#FFFFFF', fontWeight: '200' }}  className={"text-responsive"}>
+                        Joining Blueprint is the perfect way to use your skills to give back to the community. We accept applications at the beginning of every semester.
+                      </h4>
+                  </div>
+                  <div style={{ minWidth: '30vw', width: '35em', maxWidth: '75vw', paddingBottom: '1em' }}>
+                      <h5 style={{ padding: "25px 0 5px 0", "font-size": "1.2rem", color: '#FFFFFF' }}  className={"text-responsive"}>
+                        Our application for developers and designers are now closed for the 2020-2021 academic year. We will be recruiting again in Fall 2021!
+                      </h5>
+                  </div>
+              </div>
+            </Col>
+            <Col md="5" style={{ paddingLeft: '4em'}}>
+              <img src={team} style={{ paddingTop: '4em', paddingBottom: '4em', width: '30em', height: 'auto' }} />
+            </Col>
+          </Row>
         </Container>
       </Jumbotron>
       <Container fluid="md" style={{ paddingTop: "4rem", paddingBottom: '8em' }}>

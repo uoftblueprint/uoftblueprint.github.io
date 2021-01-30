a02c7a299e141c7b172b634813a6565a15f9a6ec
Varun Lodaya <varunblodaya@gmail.com>
2021-01-30 17:28:19 -0500

    Changed xs size on Col to reflect 2 people per row on mobile view

--- src/Pages/Team/Team.js
+++ src/Pages/Team/Team.js
@@ -64,7 +64,7 @@ const Team = () => {
             <span className="Header">EXECUTIVE TEAM</span>
           </h5>
           <Row>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQERSjsNVOI6cg/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=HOnA7VMQbu-HoXdRAHhDkMHb2Hzv5HxfvHzrprlxqqo"
@@ -74,7 +74,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/antoine-f/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4E03AQH7AQ2onqbW7Q/profile-displayphoto-shrink_400_400/0/1608511277502?e=1615420800&v=beta&t=1zCACzRVN_R_f0t9i9o4JecBrRsgfkhodNmZv2gV1LM"
@@ -84,7 +84,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/angela-bi-/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQHEG5tvMithXg/profile-displayphoto-shrink_400_400/0/1579562506855?e=1611792000&v=beta&t=ls2bTuPl9A7iKnVBFIVhChjidjDUMk5uf3Slzd1MOdc"
@@ -94,7 +94,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/karenjyzhao"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQGPEFE-4i6udg/profile-displayphoto-shrink_400_400/0/1552299742135?e=1611792000&v=beta&t=yoLV-L6_JA32IUVq5kclxMD9_9jag-VlQFpwH_bMOdI"
@@ -104,7 +104,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/windsorhuang/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQHMaBwyDQwDtA/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=Ytgwweh2T3_OmHHU41UgfG37G-lfQldMvXibSIw5XN8"
@@ -114,7 +114,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/oliviaoli/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQGonjCdZJiyYA/profile-displayphoto-shrink_400_400/0/1575409896752?e=1611792000&v=beta&t=WcpYh9EXTQ3UwUBY12-e_bL_Ou7hb0x4Ut8kf2f57Bk"
@@ -132,7 +132,7 @@ const Team = () => {
             <span className="Header">INTERNAL DEVELOPMENT</span>
           </h5>
           <Row>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQGPEFE-4i6udg/profile-displayphoto-shrink_400_400/0/1552299742135?e=1611792000&v=beta&t=yoLV-L6_JA32IUVq5kclxMD9_9jag-VlQFpwH_bMOdI"
@@ -142,7 +142,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/windsorhuang/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4D03AQHKNv-3M1yZCQ/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=1RumMh3UGMB7okBY0m2CKy8ryBTIcVRkrSnWlOrXx2U"
@@ -152,7 +152,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/varun-lodaya/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4D03AQHyCtOh6EnoKw/profile-displayphoto-shrink_400_400/0/1600198498959?e=1611792000&v=beta&t=Q94GK7xrYeC88HMMlxuf4p2PrzE4W-tyJ_MMzFIvIAU"
@@ -162,7 +162,7 @@ const Team = () => {
                 linkedin={"https://www.linkedin.com/in/rajvirana/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQG9he6PsQOw0g/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=celZcQNJQsrxCQ5hHQKilWZNzB4YZzN6zm7ShAAcseQ"
@@ -172,7 +172,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/elenawzy/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4E03AQE43_TmFAeAqQ/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=Q9PDMEXxi1_6pfsXUg62uDwS24583pF95rFoHCazS-Y"
@@ -182,7 +182,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/kevin-zhu-732019175/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4E03AQHGzycd8rVXEw/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=HIIaRKpuANYqu0teZtMpShRA0UMKkuW2O4azoW-GoOQ"
@@ -200,7 +200,7 @@ const Team = () => {
             <span className="Header">SHELTER MOVERS</span>
           </h5>
           <Row>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4E03AQEJEvZ4G3ocsQ/profile-displayphoto-shrink_800_800/0/1608698105884?e=1615420800&v=beta&t=DfNyW0tcTPNh3Y_6ss97DCGYRmXGKROyJEhb7nIZCbQ"
@@ -210,7 +210,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/michaelzcjia"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQHHVDmkyRnTzw/profile-displayphoto-shrink_400_400/0/1537879473606?e=1611792000&v=beta&t=qiY62Jj_1W2IQloEu0PbGlBBWoaOsBrCLnVJHMmMSgo"
@@ -220,7 +220,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/dakotamcinnis/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQHgJAl5bTl-iQ/profile-displayphoto-shrink_400_400/0/1587351527649?e=1611792000&v=beta&t=0toZeTlmYoX7oRlA5uX9FsesGbqN6oNo0ZqUB1yWF8E"
@@ -230,7 +230,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/taamannae/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4E03AQFHi70WfVD0Sw/profile-displayphoto-shrink_400_400/0/1604352087543?e=1611792000&v=beta&t=8jE50DAZyaeykbss25BHl9UYs6YIjh1zZjo7aKX1gpE"
@@ -240,7 +240,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/anita-xu/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4D03AQEPCT_g4aCDWg/profile-displayphoto-shrink_400_400/0/1544303241065?e=1611792000&v=beta&t=vRLxFEpCprBvDLNttJVb7JuSaFTjatKn_W-xCZWPp4w"
@@ -250,7 +250,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/sakina-gadriwala-14b779156/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4E03AQHZUo-pOs9Rig/profile-displayphoto-shrink_400_400/0/1596739438037?e=1611792000&v=beta&t=0DvxCB2xq3Aa_L3IC1RSy5sVfWdtNc0Z5wzLkf8ioSQ"
@@ -260,7 +260,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/danegledhill/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQFW1_fOL_PfPQ/profile-displayphoto-shrink_400_400/0/1595118555197?e=1611792000&v=beta&t=GLuszGF0BNBBxs6lQLFECxnQmTCDQCgI35dKX8-kXtA"
@@ -270,7 +270,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/raykambris"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQGmVD-sU_UI0g/profile-displayphoto-shrink_400_400/0/1587680978852?e=1611792000&v=beta&t=HG21KxpsT7TAS8Nf2G4uRfj_5CTlLwoeuEvjZW9q0IY"
@@ -280,7 +280,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/amy-p/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4E03AQGhQADpVyCXTg/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=-eHyDbzPQB89hPr49z0_ZWk6srMUyWQQQAQ4J38fehs"
@@ -290,7 +290,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/samuel-moor-smith"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4E03AQHGepLDcYoY4A/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=U9gBD9oL_Iqa-sjdf68Judhkj9lxDP22Sv0EP5k6E6Y"
@@ -308,7 +308,7 @@ const Team = () => {
             <span className="Header">MERIT AWARD</span>
           </h5>
           <Row>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4D03AQHUKKx4n3DLyg/profile-displayphoto-shrink_400_400/0/1526271577465?e=1611792000&v=beta&t=IA84KOrPNOUt0a3FuRCtcOLh-gD49nwpwiZJnLBehNA"
@@ -318,7 +318,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/trudiecheung/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQH1A3mr-Jbqbg/profile-displayphoto-shrink_400_400/0/1590598201454?e=1611792000&v=beta&t=5djtIfnMvdsu64u79-ndhNLFB5en71p7RGSvwpmL4RA"
@@ -327,7 +327,7 @@ const Team = () => {
                 role={"Project Lead"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={placeholder}
                 name={"Purnima Mehta"}
@@ -335,7 +335,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/purnimamehta"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4D03AQHDaLtYF--JrA/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=V23FG8JrDUqKz8aK_amz5-YE6-XUaOa56zggf1qW0ZM"
@@ -345,7 +345,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/baker-jackson/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={placeholder}
                 name={"Song You"}
@@ -353,7 +353,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/songeunyou/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C4D03AQE-tPMgsfyfFw/profile-displayphoto-shrink_400_400/0/1546187491813?e=1611792000&v=beta&t=v_Zj6pIOqAsywM0Zj7cwiD9BuamIbF-if-8uRfVwANY"
@@ -363,7 +363,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/alexandershih2022/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQH9HpX7uIsTAg/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=O25xaCViSyn-EWxa6SA_vAxWDPt82l0bqRq5QhIx1EI"
@@ -373,7 +373,7 @@ const Team = () => {
                 linkedin={"https://linkedin.com/in/stella-cai-77a526181/"}
               />
             </Col>
-            <Col xs={12} md={3} lg={2}>
+            <Col xs={6} md={3} lg={2}>
               <About
                 profile_img={
                   "https://media-exp1.licdn.com/dms/image/C5603AQE7PUf-3dhkgQ/profile-displayphoto-shrink_400_400/0/1553031632520?e=1611792000&v=beta&t=8I5PHdU9ulpgOuyo2PcPjMsBgAC_qhEOS5si3x9MFDc"

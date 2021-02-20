edcf0c590bf7ff807060837f7fc6b2cf39e9882e
Rajvi Rana <rajvi.rana@mail.utoronto.ca>
2021-02-20 16:39:38 -0500

    added react card for chapters page

--- src/Pages/Chapters/chapterData.js
+++ src/Pages/Chapters/chapterData.js
@@ -1,56 +1,62 @@
-import blueprint from '../../assets/mission-globe.svg'
+import blueprint from "../../assets/mission-globe.svg";
 
 export const canadaChapters = [
   {
-    title: 'University of Waterloo',
-    description: 'UWaterloo was the first Blueprint chapter, founded in 2015 🍁',
-    link: 'https://uwblueprint.org/',
+    id: 1,
+    header: "University of Waterloo",
+    description:
+      "UWaterloo was the first Blueprint chapter, founded in 2015 🍁",
+    //link: 'https://uwblueprint.org/',
     image: blueprint,
   },
   {
-    title: 'University of Toronto',
-    description: 'The second Canadian chapter to be established, started in 2020 🌿',
-    link: 'https://uoftblueprint.org/',
+    id: 2,
+    header: "University of Toronto",
+    description:
+      "The second Canadian chapter to be established, started in 2020 🌿",
+    //link: 'https://uoftblueprint.org/',
     image: blueprint,
   },
   {
-    title: 'Carleton University',
-    description: 'The third Blueprint chapter in Canada 🍂',
-    link: 'https://cublueprint.org/',
+    id: 3,
+    header: "Carleton University",
+    description: "The third Blueprint chapter in Canada 🍂",
+    //link: 'https://cublueprint.org/',
     image: blueprint,
   },
 ];
 
 export const usChapters = [
   {
-    title: 'UC Berkeley',
-    description: 'Blueprint was founded at UC Berkeley in Spring 2013.',
-    link: 'https://calblueprint.org/',
+    title: "UC Berkeley",
+    description: "Blueprint was founded at UC Berkeley in Spring 2013.",
+    link: "https://calblueprint.org/",
     image: blueprint,
   },
   {
-    title: 'UCLA',
-    description: 'Blueprint at the University of California, Los Angeles was established in 2019 🚀',
-    link: 'https://lablueprint.org/',
+    title: "UCLA",
+    description:
+      "Blueprint at the University of California, Los Angeles was established in 2019 🚀",
+    link: "https://lablueprint.org/",
     image: blueprint,
   },
   {
-    title: 'UC Irvine',
-    description: 'Our second Southern California Blueprint chapter was established in 2020 ♥️',
-    link: 'https://uciblueprint.org/#/',
+    title: "UC Irvine",
+    description:
+      "Our second Southern California Blueprint chapter was established in 2020 ♥️",
+    link: "https://uciblueprint.org/#/",
     image: blueprint,
   },
   {
-    title: 'University of Alabama',
-    description: 'Another BP chapter that was started in the US ✨',
-    link: 'https://website.sdwight.workers.dev/',
+    title: "University of Alabama",
+    description: "Another BP chapter that was started in the US ✨",
+    link: "https://website.sdwight.workers.dev/",
     image: blueprint,
   },
   {
-    title: 'University of Colorado Boulder',
-    description: 'Currently in the process of establishing in CU Boulder 💕',
-    link: 'https://calblueprint.org/chapters',
+    title: "University of Colorado Boulder",
+    description: "Currently in the process of establishing in CU Boulder 💕",
+    link: "https://calblueprint.org/chapters",
     image: blueprint,
   },
 ];
-

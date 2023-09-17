687ac6d31a97c9b9d88c5ea22170e4d8ce85c810
leowrites <75044178+leowrites@users.noreply.github.com>
2023-09-17 02:19:12 -0400

    add space to sections

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -47,61 +47,69 @@ export default function Homepage() {
       </div>
 
       {/* Values */}
-      <Container>
-        <h2 className='text-center mb-5'>Our Values</h2>
-        <Row>
-          <Col>
-            <ValueCard image={missionIcon} title={'Mission First'}>
-              <p>
-                We will always prioritize the mission first. Our main priority is doing professional
-                work for social good and public welfare.
-              </p>
-            </ValueCard>
-          </Col>
-          <Col>
-            <ValueCard image={innovationIcon} title={'Innovation'}>
-              <p>
-                We will continue to push the boundaries by combining the expertise of our developers
-                and the nonprofit to guarantee the best possible solution.
-              </p>
-            </ValueCard>
-          </Col>
-          <Col>
-            <ValueCard image={communityIcon} title={'Community'}>
-              <p>
-                We strive to develop a tight-knit community both within our club and as part of a
-                network of nonprofits.
-              </p>
-            </ValueCard>
-          </Col>
-        </Row>
-      </Container>
-
-      {/* Contact */}
-      <div id='contact-section'>
-        <Container className='p-5'>
-          <Row className='align-items-center'>
-            <Col className='text-left' xs={12} sm={6}>
-              <div id='contact-text'>
-                <h2>Contact Us!</h2>
+      <div className='section'>
+        <Container>
+          <h2 className='text-center mb-5'>Our Values</h2>
+          <Row>
+            <Col>
+              <ValueCard image={missionIcon} title={'Mission First'}>
                 <p>
-                  We are looking for nonprofits who are in need of technological solutions, as well
-                  as partners interested in supporting us and our cause.
+                  We will always prioritize the mission first. Our main priority is doing
+                  professional work for social good and public welfare.
                 </p>
-              </div>
+              </ValueCard>
             </Col>
-            <Col xs={12} sm={6}>
-              <button
-                className='contact-button'
-                onClick={() => {
-                  window.open('mailto:contact@uoftblueprint.org');
-                }}>
-                contact@uoftblueprint.org
-              </button>
+            <Col>
+              <ValueCard image={innovationIcon} title={'Innovation'}>
+                <p>
+                  We will continue to push the boundaries by combining the expertise of our
+                  developers and the nonprofit to guarantee the best possible solution.
+                </p>
+              </ValueCard>
+            </Col>
+            <Col>
+              <ValueCard image={communityIcon} title={'Community'}>
+                <p>
+                  We strive to develop a tight-knit community both within our club and as part of a
+                  network of nonprofits.
+                </p>
+              </ValueCard>
             </Col>
           </Row>
         </Container>
       </div>
+
+      {/* Contact */}
+      <ContactSection />
+    </div>
+  );
+}
+
+export function ContactSection() {
+  return (
+    <div id='contact-section'>
+      <Container>
+        <Row className='align-items-center'>
+          <Col className='text-left' xs={12} sm={6}>
+            <div id='contact-text'>
+              <h2>Contact Us!</h2>
+              <p>
+                We are looking for nonprofits who are in need of technological solutions, as well as
+                partners interested in supporting us and our cause.
+              </p>
+            </div>
+          </Col>
+          <Col xs={12} sm={6}>
+            <button
+              className='contact-button'
+              onClick={() => {
+                window.open('mailto:contact@uoftblueprint.org');
+              }}>
+              contact@uoftblueprint.org
+            </button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }

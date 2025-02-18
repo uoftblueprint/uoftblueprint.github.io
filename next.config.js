692e4329408b3be3cab9d9f4678b7614e539bca8
Jordan Janakievski <jajanak10@gmail.com>
2025-02-17 23:15:33 -0500

    Add webpack config back

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
   },
 };
 
